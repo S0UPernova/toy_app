a7dcc462afe8122e2a6834c303daaf0f074671b5
S0UPernova <mtsk321@gmail.com>
2021-09-07 18:58:27 -0700

    fixing issues with heroku deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates :name, presence: true, length: { maximum: 140}
+    validates :name, presence: true, length: { maximum: 42}
     validates :email, presence: true
 end
